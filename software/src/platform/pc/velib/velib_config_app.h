b6d9d38aeb6fdfe4f835e1bf33bb8b79ab3c54ec
Jeroen Hofstee <jhofstee@victronenergy.com>
2014-12-04 12:23:24 +0100

    remove unrelated config options

--- software/src/platform/pc/velib/velib_config_app.h
+++ software/src/platform/pc/velib/velib_config_app.h
@@ -2,30 +2,16 @@
 #define _VELIB_CONFIG_APP_H_
 
 #include <velib/velib_config_app_common.h>
-#include <velib/canhw/canhw_driver_config.h>
-
-#ifdef NMEA2K
-#define CFG_WITH_CANHW						1
-#define CFG_WITH_TIMER						1
-#define CFG_WITH_J1939_SF_MEM				1
-#define CFG_WITH_J1939_DEVICE				1
-#define CFG_WITH_FP_DEF						1
-#define CFG_WITH_VE_TIMER					1
-#define CFG_WITH_TASK						1
-#define CFG_WITH_CANHW_DRIVER				1
-#endif
 
 #define CFG_ASSERT_ENABLED					1
 #define CFG_WITH_VE_STAMP					1
 #define CFG_WITH_VE_STR						1
 
-/* log with timestamps and mk2 */
+/* log with timestamps */
 #define CFG_WITH_LOG						1
 #define CFG_LOG_STAMP_DEC					1
-#define CFG_WITH_VE_RAND					1
-#define CFG_WITH_TIMER						1
 
-/* compile in mk2 serial support */
+/* compile in serial support */
 #define CFG_WITH_SERIAL						1
 /* and open the port at startup */
 #define CFG_INIT_SERIAL						1
