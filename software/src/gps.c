b131821f0207f3c342f752d4ef3b7cf48543ae6c
malcolmh <malcolm.herring@btinternet.com>
2018-06-13 08:51:15 +0100

    full GGA parsing

--- software/src/gps.c
+++ software/src/gps.c
@@ -206,6 +206,37 @@ static void parseGGA(un8 index, char *value)
 
 	switch (index)
 	{
+	case GPS_GGA_QUALITY_INDICATOR:
+		/* 1+: fix; 0: no fix */
+		veVariantUn8(&local.fix, value[0] != '0');
+		return;
+
+	case GPS_GGA_LATITUDE:
+		errno = 0;
+		veVariantFloat(&local.latitude, toDeg((float)atof(value)));
+		if (errno)
+			veVariantInvalidate(&local.latitude);
+		return;
+
+	case GPS_GGA_LAT_NORTH_SOUTH:
+		/* North is positive, south is negative */
+		if (value[0] == 'S')
+			local.latitude.value.Float *= -1;
+		return;
+
+	case GPS_GGA_LONGITUDE:
+		errno = 0;
+		veVariantFloat(&local.longitude, toDeg((float)atof(value)));
+		if (errno)
+			veVariantInvalidate(&local.longitude);
+		return;
+
+	case GPS_GGA_LONG_EAST_WEST:
+		/* East is positive, west is negative */
+		if (value[0] == 'W')
+			local.longitude.value.Float *= -1;
+		return;
+
 	case GPS_GGA_ALTITUDE:
 		errno = 0;
 		veVariantFloat(&local.altitude, (float)atof(value));
